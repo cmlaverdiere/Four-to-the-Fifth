71ff4a2a3f49bca2664ac653f873841804aeb12d
KWhite10 <kwhite10@umbc.edu>
2014-02-25 18:43:54 -0500

    Made Machinegun
    fires a little weird

--- js/components.js
+++ js/components.js
@@ -53,3 +53,28 @@ Q.component("shotgun", {
     }
   },
 });
+
+Q.component("machinegun", {
+	  added: function() {
+	    this.entity.p.asset = "player_with_gun.png";
+	    Q.audio.play("gun_cock.wav");
+	  },
+
+	  extend: {
+	    fire: function() {
+	      if (this.p.bullets > 0){
+	        Q.audio.play("gun_shot.wav");
+	        Q.stage().insert(new Q.Bullet(
+	        { 
+	          x: this.p.x,
+	          y: this.p.y, 
+	          vx: 1000 * Math.cos(TO_RAD * (this.p.angle+90)), 
+	          vy: 1000 * Math.sin(TO_RAD * (this.p.angle+90)), 
+	        }
+	        ));
+
+	        this.p.bullets -= 1;
+	      }
+	    }
+	  },
+	});
