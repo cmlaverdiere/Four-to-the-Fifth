ba97e10a46d6d4003f659df4e45e860bdeb8358a
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-02-28 22:33:00 -0500

    Add in test sprite sheets, sample tmx, and load them in with Quintus.
    This is a big step forward.

--- js/scenes.js
+++ js/scenes.js
@@ -1,9 +1,13 @@
 // Create player scene
 Q.scene("level1", function(stage) {
-  Q.stageTMX("level1.tmx", stage);
-
   var fmod = 4;
   var frenzied_enemies = false;
+
+  Q.stageTMX("level1.tmx", stage);
+  stage.add("viewport").follow(Q("Player").first());
+
+  // I can't listen to this anymore. I need silence.
+  // Q.audio.play('test.wav', { loop: true });
 
   stage.on("enemy_killed", function(){ 
     Q.state.inc("killed", 1);
@@ -24,26 +28,4 @@ Q.scene("level1", function(stage) {
       Q.stageScene("level1", 0);
     }
   });
-
-  // Create our player
-  var player = stage.insert(new Q.Player({ bullets: 30 }));
-  Q.state.set("ammo", 30);
-
-  // Create some enemies
-  for(var i=0; i < Math.pow(Q.state.get("level")+1, 3) ; i++){
-    
-    // Shows how many enemies are left
-    Q.state.set("alive", Math.pow(Q.state.get("level")+1, 3));
-    
-    var rx = Math.random() * 3000;
-    var ry = Math.random() * 3000;
-    var rsp = Math.random() + 1;
-    var rsc = Math.random() + .5;
-    stage.insert(new Q.Enemy({ x:rx, y:ry, speed:rsp, scale:rsc, hp:(rsp * 6) }));
-  }
-
-  // I can't listen to this anymore. I need silence.
-  // Q.audio.play('test.wav', { loop: true });
-
-  stage.add("viewport").follow(player);
 });
