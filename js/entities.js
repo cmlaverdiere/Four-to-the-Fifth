63ebc8991e0ea45a6dc63d87346b69e52eb1791f
KWhite10 <kwhite10@umbc.edu>
2014-03-06 18:32:00 -0500

    machine gun
    again

--- js/entities.js
+++ js/entities.js
@@ -80,7 +80,7 @@ Q.Sprite.extend("Player", {
     // Create a block on firing so we don't shoot repeatedly when button held down.
     // Maybe make an exception for automatic guns, if ever added.
     if(Q.inputs['fire']){
-    	if(this.has("machinegun") && this.p.fire_delay > 0){
+    	if(this.p.fire_delay > 0){
     		this.p.fire_delay -= 20;
     	}
       
