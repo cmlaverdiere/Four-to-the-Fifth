54e9b59b8bd8ff0fa598e5e97140cafd5f3f4a3c
kevcoxe <kevsterkmc521@gmail.com>
2014-04-01 16:02:58 -0400

    added new UI, changed title UI changed in game UI added start_level.tmx file for a demo background
    there are some issues with loading the start_level.tmx file and the game does not always load

--- js/components.js
+++ js/components.js
@@ -16,9 +16,11 @@ Q.component("gun", {
           vy: 1000 * Math.sin(TO_RAD * (this.p.angle+90)), 
         }
         ));
-
+        
         this.p.bullets -= 1;
-        Q.state.dec("ammo", 1);
+        if(this.isA("Player")){
+          Q.state.dec("ammo", 1);
+        }
       }
     }
   },
@@ -49,7 +51,9 @@ Q.component("shotgun", {
         }
 
         this.p.bullets -= 2;
-        Q.state.dec("ammo", 2);
+        if(this.isA("Player")){
+          Q.state.dec("ammo", 2);
+        }
       }
     }
   },
@@ -75,7 +79,9 @@ Q.component("machinegun", {
         ));
 
         this.p.bullets -= 1;
-        Q.state.dec("ammo", 1);
+        if(this.isA("Player")){
+          Q.state.dec("ammo", 1);
+        }
       }
     }
   },
