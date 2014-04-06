81ad6f7934497cbcf2c87335bddcb13bb1908d8e
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-04-05 23:35:54 -0400

    Add some base classes and base colors to UI

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,46 @@
+// UI color theme
+var FG_COL = "#0099ff";
+var BG_COL = "#202020";
+
+// Base theme class for game text.
+Q.UI.FttFText = Q.UI.Text.extend("UI.FttFText", {
+  init: function(p) {
+    this._super(p, {
+      label: "Insert Text",
+      family: "nintendo_nes_font",
+      color: FG_COL,
+    });
+  }
+});
+
+// Base theme class for containers.
+Q.UI.FttFContainer = Q.UI.Container.extend("UI.FttFContainer", {
+  init: function(p) {
+    this._super(p, {
+      border: 2,
+      color: FG_COL,
+      fill: BG_COL,
+      opacity: .7,
+      radius: 3,
+    });
+  }
+});
+
+// Base theme class for buttons.
+Q.UI.FttFButton = Q.UI.Button.extend("UI.FttFButton", {
+  init: function(p) {
+    this._super(p, {
+      border: 2,
+      fill: FG_COL,
+      label: "Insert Text",
+      radius: 3,
+    });
+  }
+});
+
 Q.scene("ui", function(stage){
   // weapon container
-  var weapon_cont = stage.insert(new Q.UI.Container({
-    border: 2,
-    fill: "#888",
-    opacity: .5,
+  var weapon_cont = stage.insert(new Q.UI.FttFContainer({
     w: 200,
     h: 60,
     x: Q.width - 150,
@@ -11,11 +48,9 @@ Q.scene("ui", function(stage){
   }));
 
   // Total ammo label
-  var ammo_label = stage.insert(new Q.UI.Text({
+  var ammo_label = stage.insert(new Q.UI.FttFText({
   	size: 40,
-    x: 0,
     label: "Ammo: " + stage.options.bullets,
-
   }), weapon_cont);
 
   // Update ammo label. 
@@ -24,10 +59,7 @@ Q.scene("ui", function(stage){
   });
 
   // info container
-  var info_cont = stage.insert(new Q.UI.Container({
-    border: 2,
-    fill: "#888",
-    opacity: .5,
+  var info_cont = stage.insert(new Q.UI.FttFContainer({
     w: 200,
     h: 60,
     x: 150,
@@ -35,16 +67,14 @@ Q.scene("ui", function(stage){
   }));
 
   // health container
-  var health_cont = stage.insert(new Q.UI.Container({
-    border: 2,
-    fill: "#f00",
+  var health_cont = stage.insert(new Q.UI.FttFContainer({
+    // ...
   }), info_cont);
 
   // health label
-  var health_label = stage.insert(new Q.UI.Text({
+  var health_label = stage.insert(new Q.UI.FttFText({
   	color: "#f00",
   	size: 40,
-    x: 0,
     label: "Health: " + stage.options.hp,
   }), info_cont);
 
@@ -64,29 +94,20 @@ Q.scene("title", function(stage) {
   var title = document.getElementById("start_title");
   
   // title container
-  var title_cont = stage.insert(new Q.UI.Container({
-    border: 0,
-    opacity: .5,
-    fill: "#333",
+  var title_cont = stage.insert(new Q.UI.FttFContainer({
     x: Q.width/2,
-    y: Q.height/4,
+    y: Q.height/8,
   }));
 
   // title label
-  var start_title_label = stage.insert(new Q.UI.Text({
+  var start_title_label = stage.insert(new Q.UI.FttFText({
     label: "Four-To-The-Fifth",
-    size: 100,
-    family: "nintendo_nes_font",
-    color: "#0099ff",
-    y: 0,
+    size: 80,
+    y: 100,
   }), title_cont);
   
   // Container for start up
-  var start_cont = stage.insert(new Q.UI.Container({
-    border: 2,
-    opacity: .7,
-    fill: "#888",		// make this %80
-    radius: 3,
+  var start_cont = stage.insert(new Q.UI.FttFContainer({
     x: Q.width/2,
     y: Q.height/2,
   }));
@@ -94,10 +115,8 @@ Q.scene("title", function(stage) {
   // Button to Start Game.
   var start_btn = stage.insert(new Q.UI.Button({
     border: 2,
-    fill: "#0099ff",
-    color: "#0099ff",
+    fill: FG_COL,
     label: "Start Game",
-    radius: 3,
     y: 0,
   }, function() {
     Q.stageScene("level", 0);
@@ -107,9 +126,9 @@ Q.scene("title", function(stage) {
   // Button to show options.
   var start_options_btn = stage.insert(new Q.UI.Button({
     border: 2,
-    fill: "#0099ff",
+    fill: FG_COL,
     label: "Options",
-    color: "#0099ff",
+    color: FG_COL,
     radius: 3,
     y: 50,
   }, function() {
@@ -118,45 +137,36 @@ Q.scene("title", function(stage) {
   }), start_cont);
 
   // Container for start options
-  var start_options_cont = stage.insert(new Q.UI.Container({
-    border: 2,
+  var start_options_cont = stage.insert(new Q.UI.FttFContainer({
     hidden: true,
-    fill: "#222",
-    opacity: .7,
-    radius: 3,
     x: Q.width/2,
     y: Q.height/2,
   }));
 
   // controls label
-  var start_controls_label = stage.insert(new Q.UI.Text({
+  var start_controls_label = stage.insert(new Q.UI.FttFText({
     label: "Controls",
-    color: "#222",
     y: -80,
   }), start_options_cont);
 
   // controls label
-  var controls_label = stage.insert(new Q.UI.Text({
+  var controls_label = stage.insert(new Q.UI.FttFText({
     label: "Movement: WASD \nSwitch Weapon: NUMKEYS \nFire weapon: SPACE \nPause: BACKSPACE",
+    size: 16
   }), start_options_cont);
 
   // Button to go back to start menu.
   var return_to_start_btn = stage.insert(new Q.UI.Button({
     border: 2,
-    fill: "white",
+    fill: FG_COL,
     label: "Back",
-    radius: 3,
     y: 100,
   }, function() {
     start_options_cont.p.hidden = !(start_options_cont.p.hidden);
     start_cont.p.hidden = !(start_cont.p.hidden);
   }), start_options_cont);
 
-  //title_cont.fit(Q.width/8, Q.width);
   title_cont.fit(100, 400);
-  //start_cont.fit(10, 10);
   start_cont.fit(50, 75);
   start_options_cont.fit(10, 10);
-
 });
-
